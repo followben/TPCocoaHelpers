84d168bf5aaa5d495d1d11a68a838fd22ec66c7e
Ben Stovold <bstovold@gmail.com>
2011-10-12 22:10:38 +0100

    Adjusted titleView to use multiple textviews to fudge word-wrap on first line.

---
+++ NSString+Utils.h
@@ -0,0 +1,15 @@
+//
+//  NSString+StringUtils.h
+//  Notes
+//
+//  Created by Ben Stovold on 12/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Utils)
+
++ (NSUInteger)numberOfWordsInString:(NSString *)str;
+
+@end
---
+++ NSString+Utils.m
@@ -0,0 +1,23 @@
+//
+//  NSString+StringUtils.m
+//  Notes
+//
+//  Created by Ben Stovold on 12/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSString+Utils.h"
+
+@implementation NSString (Utils)
+
++ (NSUInteger)numberOfWordsInString:(NSString *)str {
+    __block NSUInteger count = 0;
+    [str enumerateSubstringsInRange:NSMakeRange(0, [str length])
+                            options:NSStringEnumerationByWords|NSStringEnumerationSubstringNotRequired
+                         usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                             count++;
+                         }];
+    return count;
+}
+
+@end
