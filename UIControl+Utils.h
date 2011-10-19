0ab95fb7952c64a4a23194851cc79c2069044a6e
Ben Stovold <bstovold@gmail.com>
2011-10-19 11:51:43 +0100

    Added missing disabled icon for archive and added a temp storage mechanism to Note class.

---
+++ UIControl+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UIControl+Utils.h
+//  Notes
+//
+//  Created by Ben Stovold on 19/10/2011.
+//  Copyright 2011 Ben Stovold and Gavin Conway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (Utils)
+
+- (BOOL)hasTarget:(NSObject *)target;
+
+@end
---
+++ UIControl+Utils.m
@@ -0,0 +1,23 @@
+//
+//  UIControl+Utils.m
+//  Notes
+//
+//  Created by Ben Stovold on 19/10/2011.
+//  Copyright 2011 Ben Stovold and Gavin Conway. All rights reserved.
+//
+
+#import "UIControl+Utils.h"
+
+@implementation UIControl (Utils)
+
+- (BOOL)hasTarget:(NSObject *)target
+{
+    __block BOOL matchedTarget = NO;
+    [[self allTargets] enumerateObjectsUsingBlock:^(NSObject *obj, BOOL *stop) {
+        if ([obj isEqual:target]) matchedTarget = YES;
+    }];
+     
+    return matchedTarget;
+}
+
+@end
